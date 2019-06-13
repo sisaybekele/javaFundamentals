fe0e3a5e61a3e7ae5998eca9aedca9585b7270ee
astermersha1 <astermersha1@gmail.com>
2019-06-13 13:17:18 -0500

    Created my first project

---
+++ src/com/fundamentals/java/Main.java
@@ -0,0 +1,10 @@
+package com.fundamentals.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Java");
+    }
+
+}
