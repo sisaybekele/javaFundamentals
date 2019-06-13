3a37d08db20e1bbc329e80cf0dfcffdb4a2e1cba
astermersha1 <astermersha1@gmail.com>
2019-06-13 16:03:03 -0500

    Lesson 4 ;my application overview

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,19 @@
+package com.fundamentals.java;
+/*
+*This is our first class that we create ourselves.
+ **/
+
+public class House {
+
+  String roofType;
+  int windowSize;
+  String foundation;
+  String doorColor;
+  //Our frist method (function)
+  public void doorOpenClose() {
+      System.out.println("Open my door");
+      System.out.println("Close my door");
+      System.out.println("Red");
+  }
+
+}
--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -1,10 +1,18 @@
 package com.fundamentals.java;
+/*
+* This Class is the String class of this project
+* This is the example of a multiple comment.
+ */
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to Java");
+      House myHouse = new House();
+      myHouse.doorOpenClose();
+
     }
 
 }
+
