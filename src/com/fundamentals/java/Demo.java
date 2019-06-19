82910d7109157c8571bdbca5c53eef4b95443b2f
astermersha1 <astermersha1@gmail.com>
2019-06-19 15:49:48 -0500

    Demo

--- src/com/fundamentals/java/Demo.java
+++ src/com/fundamentals/java/Demo.java
@@ -1,13 +1,51 @@
 package com.fundamentals.java;
 
+import sun.rmi.runtime.Log;
+
 public class Demo {
     //This is the main entery point of the java program
     public  static  void  main(String args[]) {
         //initialzing a variable
         int i = 5;
+        int k = 5;
+        int j = -6;
+        float i3 = 10.11F;
+        float j3 = -45.44F;
+        System.out.println("The value of i3 is  " +i3);
+        System.out.println("The value of j3 is  " +j3);
+
+
+
+
+
+        System.out.println("The value of k is "+k);
+        System.out.println("The value of j is "+j);
         //Displaying the variable
         System.out.println("The value of i is+i"+i);
          i = i+1;
         System.out.println("The value of i is+i"+i);
     }
-}
+  public static void moreDemo(){
+        long i2   = 100000000;
+        long j2 = -45000000;
+        System.out.println("The valu of i2 is " +i2);
+        System.out.println("The value of j2 is " +j2);
+      byte i = 5;
+      System.out.println("The byte value  is "+i);
+      char h ='A';
+      char m ='a';
+      System.out.println("The value of h is " +h);
+      System.out.println("The value of m is " +m);
+      Double n = 10.11111111;
+      Double b= -45.4444444;
+      System.out.println("The value of n i  "+n);
+      System.out.println("The value of n i  "+b);
+
+
+    }
+
+
+    }
+
+
+
